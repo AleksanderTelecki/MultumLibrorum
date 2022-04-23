7e07c281612970d4165c6bd826faf774e31959ef
Aleksander Telecki <129111@student.uni.opole.pl>
2022-04-23 21:01:15 +0200

    Modeling DataBase Data

--- api/models.py
+++ api/models.py
@@ -1,3 +1,131 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Author(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.company_name}"
+
+
+class BookShelf(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    publicationDate = models.DateField(blank=True, null=True)
+    availableQuantity = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    author = models.ManyToManyField(Author)
+    language = models.ManyToManyField(Language)
+    bookshelf = models.ManyToManyField(BookShelf)
+    genres = models.ManyToManyField(Genres)
+    publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True)
+    html = models.FileField(null=True, blank=True)
+    epub = models.FileField(null=True, blank=True)
+    txt = models.FileField(null=True, blank=True)
+
+    def __str__(self):
+        authors = ""
+        for item in self.author.all():
+            authors += item.name
+        return f"{self.title} | {authors}"
+
+
+class Review(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    review = models.TextField(null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.rating)
+
+
+class Order(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, blank=True, null=True)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    paidAt = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    isPaid = models.BooleanField(default=False)
+    isDelivered = models.BooleanField(default=False)
+    deliveredAt = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    discount = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    quantity = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+    image = models.URLField()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class ShippingAddress(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    postalCode = models.CharField(max_length=200, blank=True, null=True)
+    country = models.CharField(max_length=200, blank=True, null=True)
+    ShippingPrice = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return self.address
