77dc0fbf0b5e05f394802629f02a702bf9f80609
Aleksander Telecki <129111@student.uni.opole.pl>
2022-06-05 13:04:19 +0200

    JSON Web Token setup and customization

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from . import views
 
+
 urlpatterns = [
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('books/', views.APIBookListView.as_view(), name="books"),
     path('books/<str:pk>/', views.getBook, name="book")
