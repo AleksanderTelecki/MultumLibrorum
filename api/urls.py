24b7472a3f4383d26a81b57dfa0574792de49f2d
Aleksander Telecki <129111@student.uni.opole.pl>
2022-06-11 14:36:46 +0200

    Protected Routes

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
     path('users/profile/', views.getUserProfile, name="users-profile"),
+    path('users/', views.getUsers, name="users"),
+    path('', views.getRoutes, name="routes"),
     path('books/', views.APIBookListView.as_view(), name="books"),
     path('books/<str:pk>/', views.getBook, name="book")
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.models import User
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 
-from .books import books
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer, UserSerializer, UserSerializerWithToken
@@ -51,7 +50,16 @@ def getBook(request, pk):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
